f87d29bb924bd3fddecae7b25188bd2f853df08b
margarcuae <margar.tcuae@gmail.com>
2016-06-24 04:00:35 -0500

    giff

--- application/views/exito.php
+++ application/views/exito.php
@@ -2,8 +2,8 @@
         <div class="col s12 ">
           <div class="card">
             <div class="card-image">
-              <img src="<?php echo base_url() ?>material/fondo.jpg">
-              <span class="card-title">TE REGISTRASTE EXTITOSAMENTE</span>
+              <img src="<?php echo base_url() ?>material/exito.gif">
+              <span class="card-title teal lighten-4 grey-text text-darken-4">TE REGISTRASTE EXTITOSAMENTE</span>
             </div>
             <div class="card-content">
               <p>Hola !! <?php echo $this->session->username ?> ahora eres parte de la red social TORNADO.
--- application/views/muro.php
+++ application/views/muro.php
@@ -1,3 +1,6 @@
+
+
+
       <div class="row container">
         <div class="col s12">
 <?php 
@@ -7,7 +10,8 @@
           <div class="card small">
             <div class="card-image">
               <img class="" src="<?php echo base_url() ?>material/fondo.jpg">
-              <span class="card-title grey-text darken-3"><b>Posteado por: </b><?php echo $row['username'] ; ?>
+              <span class="card-title   teal-text text-darken-4">
+              <?php echo $this->session->username ?>
               </span>
             </div>
             <div class="card-content">

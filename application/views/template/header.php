814f0f7c6e4f1a8dee58976e7df0a5312c3f0679
luisbch9 <luisbch9@gmail.com>
2016-06-24 02:27:49 -0500

    testing GitHub Desk

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -9,7 +9,7 @@
 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	<script type="text/javascript" src="<?php echo base_url(); ?>material/js/jquery-3.0.0.min.js"></script>
+	<script type="text/javascript" src="<?php echo base_url(); ?>material/js/jquery-2.1.1.min.js"></script>
     <script type="text/javascript" src="<?php echo base_url(); ?>material/js/materialize.min.js"></script>
 	<title>Tornado</title>
 
